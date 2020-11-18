425b71a4e994190d7beed7547286bff890e5dc32
Kur0r0 <limaalyson@hotmail.com>
2020-11-17 22:22:30 -0300

    Separado em LSTM com e sem attention
    Separado em LSTM com e sem attention
    por motivos de consumo de ram durante o .backward() , futuramente será modificado para que o consumo de RAM seja controlado .

--- BiLSTM.py
+++ BiLSTM.py
@@ -82,6 +82,122 @@ class BiLSTM(nn.Module):
         
         # x = x.view(1 , x.shape[0] , x.shape[1] )
         #ENCODER :
+        hidden_State = torch.zeros(self.num_Layers_Encoder*2 , 1 , self.hidden_size_Encoder )
+        cell_State   = torch.zeros(self.num_Layers_Encoder*2 , 1 , self.hidden_size_Encoder )
+        
+        hidden , cell = self.encoder(x.view(1 , x.shape[0] , x.shape[1] ) , hidden_State , cell_State )
+        
+        
+        #DECODER :
+        out_seq = []
+        buffer = self.BOS.view(1,1,-1)
+        ctd = 0
+        
+        while (buffer  != self.EOS).all() and len(out_seq) < out_max_Len :
+            # print(buffer.view(1,1,-1).shape)
+            
+            
+            out , (hidden , cell) = self.decoder(buffer.view(1,1,-1) , hidden , cell) 
+            out_seq   += [out]
+            out        = heapq.nlargest(1, enumerate( buffer ) , key = lambda x : x[1])[0]
+            
+            if target != None and rd.random() < force_target_input_rate :
+                word   = self.embedding.index2word[target[ctd]]  
+            else:
+                word   = self.embedding.index2word[ out[0] ]
+        
+            buffer = torch.from_numpy( self.embedding[ word ] ).float() 
+            ctd   += 1
+            
+        
+        return torch.cat(out_seq , dim =0 )
+    
+
+    def fit(self , input_Batch , target_Batch , n , maxErro , maxAge = 1 , lossFunction = nn.CrossEntropyLoss() ,
+            lossGraphNumber = 1) :
+
+        optimizer = torch.optim.Adam(self.parameters(), n )
+        lossValue = float("inf")
+        Age = 0
+        lossList = []
+        # input_Batch = [i.view(1 , i.shape[0] , i.shape[1] ) for i in input_Batch ]
+
+        while lossValue > maxErro and Age < maxAge :
+            lossValue = 0
+            ctd = 0
+            print("Age atual {}".format(Age))
+            for x,y in zip(input_Batch , target_Batch ) :
+                if type(y) != type(torch.tensor([1])) :
+                    x = torch.from_numpy(x).float()
+                    y = torch.from_numpy(y).float()
+                div = len(y)
+                                
+                out = self.forward_fit(x ,out_max_Len = y.shape[0] ,target = y )
+
+                print("Age atual {} , ctd atual {}\nout.shape = {} , y.shape = {}".format(Age ,ctd ,out.shape , y.shape))
+                loss = lossFunction(out , y)/div
+                lossValue += loss.item()
+                print("Pré backward")
+                loss.backward()
+                print("Pós backward")
+                optimizer.step()
+                optimizer.zero_grad()
+                ctd += 1
+            Age += 1
+            lossValue = lossValue/len(target_Batch)
+            lossList.append(lossValue)
+        
+        plt.plot(range(1 , Age + 1) , lossList)
+        if lossGraphNumber != 1 :
+            plt.savefig("/content/drive/My Drive/Aprender a Usar A nuvem_Rede-Neural/{}_BiLSTM_LossInTrain_Plot.png".format(lossGraphNumber) )
+            plt.savefig("/content/drive/My Drive/Aprender a Usar A nuvem_Rede-Neural/{}_BiLSTM_LossInTrain_Plot.pdf".format(lossGraphNumber) )
+        else :
+            plt.savefig("/content/drive/My Drive/Aprender a Usar A nuvem_Rede-Neural/BiLSTM_LossInTrain_Plot.png")
+            plt.savefig("/content/drive/My Drive/Aprender a Usar A nuvem_Rede-Neural/BiLSTM_LossInTrain_Plot.pdf")
+        plt.show()
+
+class BiLSTM_Attention(nn.Module):
+    def __init__(self , input_dim , hidden_size_Encoder , num_Layers_Encoder ,
+            hidden_size_Decoder , num_Layers_Decoder , num_classes , embedding , EOS_Vector ):
+        super(BiLSTM , self).__init__()
+        
+        self.input_dim   = input_dim
+        
+        self.hidden_size_Encoder = hidden_size_Encoder
+        self.num_Layers_Encoder  = num_Layers_Encoder
+        self.hidden_size_Decoder = hidden_size_Decoder
+        self.num_Layers_Decoder  = num_Layers_Decoder
+
+        self.encoder   = Encoder( input_dim , hidden_size_Encoder , num_Layers_Encoder)
+        self.decoder   = Decoder(  input_dim , hidden_size_Decoder , num_Layers_Decoder , num_classes)
+        self.attention = nn.Linear(2*hidden_size_Encoder*num_Layers_Encoder + 2*hidden_size_Decoder*num_Layers_Decoder , 1 )
+        #    hidden_size_Decoder*num_Layers_Decoder*2 )
+        self.embedding = embedding
+        self.EOS = EOS_Vector
+        self.BOS = -EOS_Vector
+
+    def forward(self , x , out_max_Len = 150) :
+        # h0 = torch.zeros(self.num_Layers*2 , x.size(0) , self.hidden_size )
+        # c0 = torch.zeros(self.num_Layers*2 , x.size(0) , self.hidden_size )
+        
+        hidden_State , cell_State = self.encoder(x)
+
+        seq = []
+        buffer = self.BOS
+        while (buffer != self.EOS).all() and seq.shape[0] < out_max_Len :
+            out , hidden_State , cell_State = self.decoder(buffer , hidden_State , cell_State)
+            out    = heapq.nlargest(1, enumerate( buffer ) , key = lambda x : x[1])[0]
+            word   = self.embedding.index2word[ out[0] ]
+            buffer = torch.from_numpy( self.embedding[ word ] ).float()
+            seq   += [word]
+        
+        return seq
+        return self.linear(x[ : , -1 , : ])
+
+    def forward_fit(self , x , out_max_Len = 150 ,target = None , force_target_input_rate = 0.5) :
+        
+        # x = x.view(1 , x.shape[0] , x.shape[1] )
+        #ENCODER :
         hidden_State = [torch.zeros(self.num_Layers_Encoder*2 , 1 , self.hidden_size_Encoder )]
         cell_State   = [torch.zeros(self.num_Layers_Encoder*2 , 1 , self.hidden_size_Encoder )]
         print("pré lista de estados")
@@ -173,3 +289,4 @@ class BiLSTM(nn.Module):
             plt.savefig("/content/drive/My Drive/Aprender a Usar A nuvem_Rede-Neural/BiLSTM_LossInTrain_Plot.png")
             plt.savefig("/content/drive/My Drive/Aprender a Usar A nuvem_Rede-Neural/BiLSTM_LossInTrain_Plot.pdf")
         plt.show()
+
