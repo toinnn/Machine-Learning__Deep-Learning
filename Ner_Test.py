b0785f49bba5497bb6dbcc62e144e26d876cede7
Kur0r0 <limaalyson@hotmail.com>
2020-11-18 19:34:26 -0300

    método que separa json em classes
    método recebe uma lista de jsons e uma key , então retorna uma lista de  tensores das classes p/ cada arquivo json existente na lista e um dicionário informando o indice que representa cada uma das classes existentes(quando existentes) . O maior índice é dedicado a não aparição de classe alguma e não está listado no diconário de classes , mas é usado nos tensores dentro da lista .

--- Ner_Test.py
+++ Ner_Test.py
@@ -54,10 +54,30 @@ if __name__ == "__main__" :
             print("Entrou no não existe a key ")
             return tensor([gensimWorVec.vocab["<key_Vazia>"].index])
 
+    def jsonList2classes(jsList , key):
+        classes = {}
+        classifiedList = []
+        for js in jsList :
+            try :
+                if type(js[key])==type([]) :
+                    Classified = []
+                    for content in js[key] :
+                        if content not in classes :
+                            classes += {content:len(classes.keys())}
+                        Classified += [classes[content]]
+                else :
+                    if js[key] not in classes :
+                        classes += {js[key]:len(classes.keys())}
+                    Classified  = [classes[js[key]]]
+                classifiedList += [torch.tensor(Classified)]
+            except :
+                classifiedList += [-1]
+        
+        return classes , [ i if i!=-1 else torch.tensor([len(classes.keys())]) for i in classifiedList  ]
     # -47 : Key não existente ; -127 : separador entre targets diferentes ; -79 : fora do vocab ; 23 : End-Of-Sentence
         
 
-    embeddingPath = "EmbeddingBaixados\\Word2Vec_skip_s50\\skip_s50.txt"
+    # embeddingPath = "EmbeddingBaixados\\Word2Vec_skip_s50\\skip_s50.txt"
     # inputPath  ="leNer-Dataset\\raw-Text\\"
     # outputPath = "leNer-Dataset\\Json-Ner\\"
 
@@ -68,7 +88,28 @@ if __name__ == "__main__" :
     # ark = [open( i , "r" , encoding = "utf8").read() for i in ark_Input ]
     # ark_Target = [json.load(open(i , "rb")) for i in ark_Output]
 
-
+    # materias = []
+    # for i in ark_Target :
+    #     try :
+    #         if type(i["materias"]) == type([]) :
+    #             for j in i["materias"] :
+    #                 # materias += [j]
+    #                 if j not in materias :
+    #                     materias += [j]
+    #         else :
+    #             if i["materias"] not in materias :
+    #                 materias += [i["materias"]]
+    #     except :
+    #         pass
+    # materias = ['Direito Civil', 'Direito Processual Civil', 'Direito Administrativo', 'Direito Constitucional',
+    #     'Direito Penal', 'Direito Processual do Trabalho', 'Direito do Trabalho', 'Direito Penal Militar',
+    #     'Direito Processual Penal', 'Direito Eleitoral', 'Direito Processual Penal Militar', 'Direito Previdenciário', '']
+    # print("as materias únicas são " , materias ) #12 classes e uma indicando que não tem
+    # print(ark_Target[0]["materias"])
+    # teste = "palavra"
+    # print(teste)
+    # teste = [teste] if type(teste)==type("exe") 
+    # print(teste)
     # embed = skip_gram(ark)
     # embed.tokenize()
 
@@ -81,7 +122,7 @@ if __name__ == "__main__" :
 
     # print(embed.trainVectors(vectorDim ,n,momentum, maxAge , maxErro ,saveNewPairs = True , device = torch.device("cpu") ) )
 
-    # basePath = "C:\\Users\\limaa\\PythonProjects\\VsCodePython\\KPMG\\"
+    basePath = "C:\\Users\\limaa\\PythonProjects\\VsCodePython\\KPMG\\"
     # saveSkipGramFile = basePath + "1-n={}_dim={}_maxAge={}_momentum={}_maxErro={}.savedSkipGram".format(
     #     n,vectorDim,maxAge , momentum ,maxErro)
     # svFile = open(saveSkipGramFile , "wb")
@@ -101,9 +142,10 @@ if __name__ == "__main__" :
     # wv["<EOS>"] = np.ones([1,50])*23
 
     # print(ark[0] ,"\n" , ark_Target[0]["materias"]  )
-
+    
     # ark        = [ sen2vec(wv , i , 50 ) for i in ark ]
-    # ark_Target = [ json2idx(js , "materias"  , wv ) for js in ark_Target ]
+    # ark_Target = [ json2idx(js , "materias" , wv) for js in ark_Target ]
+    # classes , ark_Target = jsonList2classes(ark_Target , "materias")
 
     # print(ark[0] ,"\n" , ark_Target[0]  )
     # wv["<EOS>"] = np.ones(50)*-79
@@ -111,20 +153,22 @@ if __name__ == "__main__" :
     # pickle.dump(wv , open("wv_W2Vec.pickle","wb"))
     # pickle.dump(ark , open("ark_W2Vec.pickle","wb"))
     # pickle.dump(ark_Target , open("ark_Target_W2Vec.pickle","wb"))
+    # pickle.dump(classes , open("classesTarget_Entity:materias_W2Vec.pickle","wb"))
 
     #O Git-Hub não permite que seja upado um arquivo tão grande quanto wv_W2Vec.pickle , então pode descomentar a linha 110 para gerar
     #o wv serializado e depois comentar as linhas de manipulação do wv e descomentar a linha abaixo para conseguir uma execução + rápida
     wv = pickle.load(open("wv_W2Vec.pickle","rb"))
     ark = pickle.load(open("ark_W2Vec.pickle","rb"))
     ark_Target = pickle.load(open("ark_Target_W2Vec.pickle","rb"))
+    classes = pickle.load(open("classesTarget_Entity:materias_W2Vec.pickle","rb"))
     
     # print(wv["<EOS>"])
     print(len(wv.vocab))
     # model = Tener(50 , 5 ,5 , 6 , 6 ,wv , np.ones([1,50])*23 ) 
     # model.fit(ark , ark_Target , 10 , 0.005 , n = 0.05 , lossGraphNumber = 1 )
     # pickle.dump(model , open("1_TenerTreinado_maxAge=10_maxErro=0.005_n=0.05.pickle" , "wb"))
-    lstm = BiLSTM(50 ,100 , 1, 100,1 , len(wv.vocab) , wv , torch.ones([1,50])*23 )
-    lstm.fit(ark, ark_Target , 0.05 ,0.06 , 20 )
+    lstm = BiLSTM(50 ,100 , 1, 100,1 , len(classes.keys()) + 1, wv , torch.ones([1,50])*23 )
+    # lstm.fit(ark, ark_Target , 0.05 ,0.06 , 20 )
     # lstm.fit([i.view(1 , i.shape[0] , i.shape[1] ) for i in ark ], ark_Target , 0.05 ,0.06 , 10 )
     # pickle.dump(lstm , open("/content/drive/My Drive/Aprender a Usar A nuvem_Rede-Neural/lstm_n=0.05_maxErro=0.06_maxAge=10.pickle","wb"))
     print("len Ark = {}  , len target = {}".format(len(ark) , len(ark_Target)))
