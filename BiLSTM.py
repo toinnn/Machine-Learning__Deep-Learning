91668359a6b502e4eb80d7c94869d7be05a3acda
Kur0r0 <limaalyson@hotmail.com>
2020-12-01 20:53:30 -0300

    Update BiLSTM.py

--- BiLSTM.py
+++ BiLSTM.py
@@ -254,9 +254,9 @@ class BiLSTM_Attention(nn.Module):
             #ATTENTION :
             # att_hidden = sum( self.attention(torch.cat((i.view(1 , -1 ) , hidden.view(1 , -1 ) ) , dim = 1) ).view(self.num_Layers_Decoder*2 , 1 ,self.input_dim) for i in hidden_State )
             # att_cell   = sum( self.attention(torch.cat((i.view(1 , -1 ) , cell.view(1 , -1 ) ) , dim = 1) ).view(self.num_Layers_Decoder*2 , 1 ,self.input_dim) for i in cell_State )
-            print("hidden_State.shape = " , hidden_State.shape)
+            print("hidden_State.shape = " , hidden_State[0].shape)
             att_hidden  = self.attention( hidden_State) 
-            att_cell    = self.attention( cell_State )
+            att_cell    = self.attention( tuple(cell_State) )
             # print(att_hidden[0])
             # print("pré SoftMax")
             att_hidden = F.softmax(  att_hidden , dim = 0)
